c87c690cb99a925a8d49f27f48ad1323bbcbbb5e
dallinw <dallinjwilliams@gmail.com>
2020-03-24 22:19:03 -0400

    Updates

--- precache-manifest.6ef0e76d5ef6339eca5c88c6af6c32b9.js
+++ precache-manifest.6ef0e76d5ef6339eca5c88c6af6c32b9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ed4fdb1e62290d7da84a2d3c0f51a16d",
+    "revision": "c7c2aa1840c2d068e96de1af3e18cfa7",
     "url": "/atomizer/index.html"
   },
   {
-    "revision": "4e31a88b6626c65ee28e",
+    "revision": "e4659b2fac229766e5ce",
     "url": "/atomizer/static/css/main.16f7795f.chunk.css"
   },
   {
-    "revision": "322f0d839b92d722147a",
-    "url": "/atomizer/static/js/2.2d92fbdc.chunk.js"
+    "revision": "8f829fe580eaa14c54e9",
+    "url": "/atomizer/static/js/2.cfa3033e.chunk.js"
   },
   {
     "revision": "a3d9eabd6f36d165e90a649a8ffbb585",
-    "url": "/atomizer/static/js/2.2d92fbdc.chunk.js.LICENSE.txt"
+    "url": "/atomizer/static/js/2.cfa3033e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4e31a88b6626c65ee28e",
-    "url": "/atomizer/static/js/main.fc23f953.chunk.js"
+    "revision": "e4659b2fac229766e5ce",
+    "url": "/atomizer/static/js/main.5633012b.chunk.js"
   },
   {
     "revision": "e0d92a4bfdcf6938d3bd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atomizer/precache-manifest.f6b4735e01ea2e63bbad50cbf553ddd6.js"
+  "/atomizer/precache-manifest.6ef0e76d5ef6339eca5c88c6af6c32b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/atomizer/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
