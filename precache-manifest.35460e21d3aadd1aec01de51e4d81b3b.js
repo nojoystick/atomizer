4c4b1674b34f21caa6427b073fc9675f27c0e3d7
dallinw <dallinjwilliams@gmail.com>
2020-04-24 22:24:23 +0000

    Deploying to gh-pages from  @ 63ee1059f72580f4ad13974edbc6e461fb744831 🚀

---
+++ precache-manifest.35460e21d3aadd1aec01de51e4d81b3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e22be3abb2b979865c7f7527be74cb",
+    "url": "/atomizer/index.html"
+  },
+  {
+    "revision": "6ef4fc69245cc78f2139",
+    "url": "/atomizer/static/css/2.550f20f8.chunk.css"
+  },
+  {
+    "revision": "5c5e95aae3b877aaaa82",
+    "url": "/atomizer/static/css/main.ff90268e.chunk.css"
+  },
+  {
+    "revision": "6ef4fc69245cc78f2139",
+    "url": "/atomizer/static/js/2.a429afcc.chunk.js"
+  },
+  {
+    "revision": "ded8d165dd0e38d9a5d8a677c36b3065",
+    "url": "/atomizer/static/js/2.a429afcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5e95aae3b877aaaa82",
+    "url": "/atomizer/static/js/main.671d37c0.chunk.js"
+  },
+  {
+    "revision": "e0d92a4bfdcf6938d3bd",
+    "url": "/atomizer/static/js/runtime-main.94fcc704.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atomizer/precache-manifest.3efa584c1de4d2fc3b5c4869531b68e4.js"
+  "/atomizer/precache-manifest.35460e21d3aadd1aec01de51e4d81b3b.js"
 );
 
 self.addEventListener('message', (event) => {
