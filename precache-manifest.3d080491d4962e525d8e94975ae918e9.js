53425797c93bc9dac7992a5d707aa99469d1a768
williamsdj6 <dallin.j.williams@leidos.com>
2020-05-01 21:36:28 -0400

    Updates

---
+++ precache-manifest.3d080491d4962e525d8e94975ae918e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e86249c8a1c40a5c4fd659e33e26ede",
+    "url": "/atomizer/index.html"
+  },
+  {
+    "revision": "60a2ec32cc48afbace3a",
+    "url": "/atomizer/static/css/main.fa6d4d88.chunk.css"
+  },
+  {
+    "revision": "bbf7951f2d64d66131a3",
+    "url": "/atomizer/static/js/2.582431fd.chunk.js"
+  },
+  {
+    "revision": "92db77eb223eb97871b4a78934f71440",
+    "url": "/atomizer/static/js/2.582431fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a2ec32cc48afbace3a",
+    "url": "/atomizer/static/js/main.a45ab63b.chunk.js"
+  },
+  {
+    "revision": "e0d92a4bfdcf6938d3bd",
+    "url": "/atomizer/static/js/runtime-main.94fcc704.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/atomizer/precache-manifest.076e1d48885ab1305f47ee3cad83bd11.js"
+  "/atomizer/precache-manifest.3d080491d4962e525d8e94975ae918e9.js"
 );
 
 self.addEventListener('message', (event) => {
